583f3c368468607f8786b6057afee28efbafb603
VytasS <113248280+vstuopis@users.noreply.github.com>
2023-03-23 21:34:02 -0400

    Adding updated ERD Image and SQL Source File.

--- ERD SQL Source File.sql
+++ ERD SQL Source File.sql
@@ -1,31 +1,34 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/KimmqP
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-CREATE TABLE `NFL_Game_Data` (
-    `Date` string  NOT NULL ,
-    `Away Team ID` string  NOT NULL ,
-    `Home Team ID` string  NOT NULL ,
-    `Turnovers_away` integer  NOT NULL ,
-    `Turnovers_home` integer  NOT NULL ,
-    `Possession_away` string  NOT NULL ,
-    `Possession_home` string  NOT NULL ,
-    `Score_away` integer  NOT NULL ,
-    `Score_home` integer  NOT NULL 
+CREATE TABLE "NFL_Game_Data" (
+    "date" DATE   NOT NULL,
+    "away" VARCHAR   NOT NULL,
+    "home" VARCHAR   NOT NULL,
+    "turnovers_away" integer   NOT NULL,
+    "turnovers_home" integer   NOT NULL,
+    "possession_away" VARCHAR   NOT NULL,
+    "possession_home" VARCHAR   NOT NULL,
+    "score_away" integer   NOT NULL,
+    "score_home" integer   NOT NULL,
+    CONSTRAINT "pk_NFL_Game_Data" PRIMARY KEY (
+        "date","away","home"
+     )
 );
 
-CREATE TABLE `NFL_Team_IDs` (
-    `Team_ID` integer  NOT NULL ,
-    `Team_Name` string  NOT NULL ,
-    `Team_City` string  NOT NULL 
+CREATE TABLE "NFL_Team_IDs" (
+    "Team_ID" INT   NOT NULL,
+    "Team_Name" VARCHAR   NOT NULL,
+    "Team_City" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_NFL_Team_IDs" PRIMARY KEY (
+        "Team_Name"
+     )
 );
 
-ALTER TABLE `NFL_Game_Data` ADD CONSTRAINT `fk_NFL_Game_Data_Away Team ID` FOREIGN KEY(`Away Team ID`)
-REFERENCES `NFL_Team_IDs` (`Team_ID`);
+ALTER TABLE "NFL_Game_Data" ADD CONSTRAINT "fk_NFL_Game_Data_away" FOREIGN KEY("away")
+REFERENCES "NFL_Team_IDs" ("Team_Name");
 
-ALTER TABLE `NFL_Game_Data` ADD CONSTRAINT `fk_NFL_Game_Data_Home Team ID` FOREIGN KEY(`Home Team ID`)
-REFERENCES `NFL_Team_IDs` (`Team_ID`);
+ALTER TABLE "NFL_Game_Data" ADD CONSTRAINT "fk_NFL_Game_Data_home" FOREIGN KEY("home")
+REFERENCES "NFL_Team_IDs" ("Team_Name");
 
